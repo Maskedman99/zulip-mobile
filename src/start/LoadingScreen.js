d4b386ffc0e34eff1f7940448d82c858e8bb0ac0
Rohit <rohitmpaul@gmail.com>
2020-02-05 20:07:09 +0530

    ui: Fix color of statusbar in loading screen.
    In android the color of status-bar doesn't match with the background
    color in loading screen. This is fixed by adding ZulipStatusBar
    component with the props backgroundColor={BRAND_COLOR}
    
    Fixes: #3860.

--- src/start/LoadingScreen.js
+++ src/start/LoadingScreen.js
@@ -1,7 +1,6 @@
 /* @flow strict-local */
 import React, { PureComponent } from 'react';
 import { StyleSheet, View } from 'react-native';
-import Color from 'color';
 
 import { BRAND_COLOR } from '../styles';
 import { LoadingIndicator, ZulipStatusBar } from '../common';
@@ -19,7 +18,7 @@ export default class LoadingScreen extends PureComponent<{||}> {
   render() {
     return (
       <View style={styles.center}>
-        <ZulipStatusBar backgroundColor={Color(BRAND_COLOR).lighten(0.1)} />
+        <ZulipStatusBar backgroundColor={BRAND_COLOR} />
         <LoadingIndicator color="0, 0, 0" size={80} showLogo />
       </View>
     );
